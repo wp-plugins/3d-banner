1d5d92ad7298b68614365769636a5c232aab3b44
johnsmith48 <johnsmith48@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-02-22 11:43:37 +0000

    git-svn-id: https://plugins.svn.wordpress.org/3d-banner/trunk@349121 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ 3d-banner-fx.php
@@ -0,0 +1,186 @@
+<?php
+/*
+Plugin Name: 3D Banner
+Plugin URI: http://www.flashxml.net/3d-banner.html
+Description: Probably the best 3D Banner on the web. Fully XML customizable, without any Flash knowledge. And it's free!
+Version: 0.6.0
+Author: johnsmith48
+Author URI: http://www.flashxml.net/
+License: GPL2
+*/
+
+/* start global parameters */
+	$bannerfx_params = array(
+		'count'	=> 0, // number of 3D Banner FX embeds
+		'read_settings_from_url' => false, // true only when the settings XML file must be read via HTTP (is generated dynamically or is hosted on another domain)
+	);
+/* end global parameters */
+
+/* start client side functions */
+	function bannerfx_get_embed_code($bannerfx_attributes) {
+		global $bannerfx_params;
+		$bannerfx_params['count']++;
+
+		$plugin_dir = get_option('bannerfx_path');
+		if ($plugin_dir === false) {
+			$plugin_dir = 'flashxml/3d-banner-fx';
+		}
+		$plugin_dir = trim($plugin_dir, '/');
+
+		$settings_file_name = !empty($bannerfx_attributes[2]) ? html_entity_decode(urldecode($bannerfx_attributes[2])) : 'settings.xml';
+
+		$settings_wp_content_prefix = $bannerfx_params['read_settings_from_url'] && strtolower(ini_get('allow_url_fopen')) == 'on' || strtolower(ini_get('allow_url_fopen')) == '1' ? WP_CONTENT_URL : WP_CONTENT_DIR;
+		$settings_path = "{$settings_wp_content_prefix}/{$plugin_dir}/{$settings_file_name}";
+
+		$width = $height = 0;
+
+		if (function_exists('simplexml_load_file') && ($settings_wp_content_prefix == WP_CONTENT_URL || $settings_wp_content_prefix == WP_CONTENT_DIR && file_exists($settings_path))) {
+			$data = simplexml_load_file($settings_path);
+			if ($data) {
+				$width_attributes_array = $data->General_Properties->componentWidth->attributes();
+				$width = !empty($width_attributes_array) ? (int)$width_attributes_array['value'] : 0;
+				$height_attributes_array = $data->General_Properties->componentHeight->attributes();
+				$height = !empty($height_attributes_array) ? (int)$height_attributes_array['value'] : 0;
+			}
+		}
+
+		if (!($width > 0 && $height > 0)) {
+			if ((int)$bannerfx_attributes[4] > 0 && (int)$bannerfx_attributes[6] > 0) {
+				$width = (int)$bannerfx_attributes[4];
+				$height = (int)$bannerfx_attributes[6];
+			} else {
+				return '<!-- invalid 3D Banner FX width and / or height in plugin parameters -->';
+			}
+		}
+
+		$swf_embed = array(
+			'width' => $width,
+			'height' => $height,
+			'text' => isset($bannerfx_attributes[7]) ? trim($bannerfx_attributes[7]) : '',
+			'component_path' => WP_CONTENT_URL . "/{$plugin_dir}/",
+			'swf_name' => '3DBannerFX.swf',
+		);
+		$swf_embed['swf_path'] = $swf_embed['component_path'].$swf_embed['swf_name'];
+
+		if (!is_feed()) {
+			$embed_code = '<div id="banner-fx'.$bannerfx_params['count'].'">'.$swf_embed['text'].'</div>';
+			$embed_code .= '<script type="text/javascript">';
+			$embed_code .= "swfobject.embedSWF('{$swf_embed['swf_path']}', 'banner-fx{$bannerfx_params['count']}', '{$swf_embed['width']}', '{$swf_embed['height']}', '9.0.0.0', '', { folderPath: '{$swf_embed['component_path']}'".($settings_file_name != 'settings.xml' ? ", settingsXML: '".urlencode($settings_file_name)."'" : '')." }, { scale: 'noscale', salign: 'tl', wmode: 'transparent', allowScriptAccess: 'sameDomain', allowFullScreen: true }, {});";
+			$embed_code.= '</script>';
+		} else {
+			$embed_code = '<object width="'.$swf_embed['width'].'" height="'.$swf_embed['height'].'">';
+			$embed_code .= '<param name="movie" value="'.$swf_embed['swf_path'].'"></param>';
+			$embed_code .= '<param name="scale" value="noscale"></param>';
+			$embed_code .= '<param name="salign" value="tl"></param>';
+			$embed_code .= '<param name="wmode" value="transparent"></param>';
+			$embed_code .= '<param name="allowScriptAccess" value="sameDomain"></param>';
+			$embed_code .= '<param name="allowFullScreen" value="true"></param>';
+			$embed_code .= '<param name="sameDomain" value="true"></param>';
+			$embed_code .= '<param name="flashvars" value="folderPath='.$swf_embed['component_path'].($settings_file_name != 'settings.xml' ? '&settingsXML='.urlencode($settings_file_name) : '').'"></param>';
+			$embed_code .= '<embed type="application/x-shockwave-flash" width="'.$swf_embed['width'].'" height="'.$swf_embed['height'].'" src="'.$swf_embed['swf_path'].'" scale="noscale" salign="tl" wmode="transparent" allowScriptAccess="sameDomain" allowFullScreen="true" flashvars="folderPath='.$swf_embed['component_path'].($settings_file_name != 'settings.xml' ? '&settingsXML='.urlencode($settings_file_name) : '').'"';
+			$embed_code .= '></embed>';
+			$embed_code .= '</object>';
+		}
+
+		return $embed_code;
+	}
+
+	function bannerfx_filter_content($content) {
+		return preg_replace_callback('|\[3d-banner-fx\s*(settings="([^"]+)")?\s*(width="([0-9]+)")?\s*(height="([0-9]+)")?\s*\](.*)\[/3d-banner-fx\]|i', 'bannerfx_get_embed_code', $content);
+	}
+
+	function bannerfx_echo_embed_code($settings_xml_path = '', $div_text = '', $width = 0, $height = 0) {
+		echo bannerfx_get_embed_code(array(2 => $settings_xml_path, 7 => $div_text, 4 => $width, 6 => $height));
+	}
+
+	function bannerfx_load_swfobject_lib() {
+		wp_enqueue_script('swfobject');
+	}
+/* end client side functions */
+
+/* start admin section functions */
+	function bannerfx_admin_menu() {
+		add_options_page('3D Banner FX Options', '3D Banner FX', 'manage_options', 'bannerfx', 'bannerfx_admin_options');
+	}
+
+	function bannerfx_admin_options() {
+		  if (!current_user_can('manage_options'))  {
+	    wp_die(__('You do not have sufficient permissions to access this page.'));
+	  }
+
+	  $bannerfx_default_path = get_option('bannerfx_path');
+	  if ($bannerfx_default_path === false) {
+	  	$bannerfx_default_path = 'flashxml/3d-banner-fx';
+	  }
+?>
+<div class="wrap">
+	<h2>3D Banner FX</h2>
+	<form method="post" action="options.php">
+		<?php wp_nonce_field('update-options'); ?>
+
+		<table class="form-table">
+			<tr valign="top">
+				<th scope="row" style="width: 40em;">SWF and assets path is <?php echo basename(WP_CONTENT_DIR); ?>/</th>
+				<td><input type="text" style="width: 25em;" name="bannerfx_path" value="<?php echo $bannerfx_default_path; ?>" /></td>
+			</tr>
+		</table>
+		<input type="hidden" name="action" value="update" />
+		<input type="hidden" name="page_options" value="bannerfx_path" />
+		<p class="submit">
+			<input type="submit" class="button-primary" value="<?php _e('Save Changes') ?>" />
+		</p>
+	</form>
+</div>
+<?php
+	}
+/* end admin section functions */
+
+/* start widget class */
+class BannerFXWidget extends WP_Widget {
+	function BannerFXWidget() {
+		parent::WP_Widget(false, $name = '3D Banner FX');
+	}
+
+	function widget($args, $instance) {
+		echo $before_widget;
+		echo bannerfx_echo_embed_code($instance['settings_xml_path'], $instance['div_text'], $instance['width'], $instance['height']);
+		echo $after_widget;
+	}
+
+	function update($new_instance, $old_instance) {
+		$instance = $old_instance;
+		$instance['settings_xml_path'] = $new_instance['settings_xml_path'];
+		$instance['div_text'] = $new_instance['div_text'];
+		$instance['width'] = strip_tags($new_instance['width']);
+		$instance['height'] = strip_tags($new_instance['height']);
+		return $instance;
+	}
+
+	function form($instance) {
+		$settings_xml_path = esc_attr($instance['settings_xml_path']);
+		$div_text = esc_attr($instance['div_text']);
+		$width = esc_attr($instance['width']);
+		$height = esc_attr($instance['height']);
+
+		$plugin_dir = get_option('bannerfx_path');
+		if ($plugin_dir === false) {
+			$plugin_dir = 'flashxml/3d-banner-fx';
+		}
+?>
+            <p><label for="<?php echo $this->get_field_id('settings_xml_path'); ?>"><?php _e('Settings XML in:'); ?> <?php echo basename(WP_CONTENT_DIR)."/{$plugin_dir}/"; ?> <input class="widefat" id="<?php echo $this->get_field_id('settings_xml_path'); ?>" name="<?php echo $this->get_field_name('settings_xml_path'); ?>" type="text" value="<?php echo $settings_xml_path; ?>" /></label></p>
+            <p><label for="<?php echo $this->get_field_id('div_text'); ?>"><?php _e('Alternative content:'); ?> <textarea class="widefat" id="<?php echo $this->get_field_id('div_text'); ?>" name="<?php echo $this->get_field_name('div_text'); ?>"><?php echo $div_text; ?></textarea></label></p>
+            <p><label for="<?php echo $this->get_field_id('width'); ?>"><?php _e('Width:'); ?> <input id="<?php echo $this->get_field_id('width'); ?>" name="<?php echo $this->get_field_name('width'); ?>" type="text" value="<?php echo $width; ?>" /></label></p>
+            <p><label for="<?php echo $this->get_field_id('height'); ?>"><?php _e('Height:'); ?> <input id="<?php echo $this->get_field_id('height'); ?>" name="<?php echo $this->get_field_name('height'); ?>" type="text" value="<?php echo $height; ?>" /></label></p>
+<?php
+	}
+}
+/* end widget class */
+
+/* start hooks */
+	add_filter('the_content', 'bannerfx_filter_content');
+	add_action('init', 'bannerfx_load_swfobject_lib');
+	add_action('admin_menu', 'bannerfx_admin_menu');
+	add_action('widgets_init', create_function('', 'return register_widget("BannerFXWidget");'));
+/* end hooks */
+
+?>
---
+++ readme.txt
@@ -0,0 +1,47 @@
+=== 3D Banner FX ===
+Contributors: flashxml
+Tags: flash, 3d, banner, images, video, free, widget, css, effects, animation, text, html
+Requires at least: 2.8.0
+Tested up to: 3.0.5
+Stable tag: trunk
+
+Probably the best 3D Banner on the web. Fully XML customizable, without any Flash knowledge. And it's free!
+
+== Description ==
+
+You can integrate it in any website for free without any Flash knowledge. Customizable width and height of the overall banner, up to 1680 x 1050 pixels. It has awesome 3D image transitions with lots of shade and background properties. The text is HTML/CSS formatted. The navigation buttons can be placed o various positions and are skinnable. A lot of other properties in the Live Demo.
+
+== Installation ==
+
+Make sure your Wordpress version is greater than 2.8 and your hosting provider is using PHP5.
+
+1. There are two files to download: [WordPress Plugin](http://downloads.wordpress.org/plugin/3d-banner-fx.zip "3D Banner FX Plugin") (that you have to install and activate) & [Free package](http://www.flashxml.net/free/download/3d-banner.zip "3D Banner FX")
+2. Create a new folder inside your **wp-content** folder called **flashxml**, inside this folder create a new one called **3d-banner-fx** and copy the content of the **free package** there
+3. If you copied the **free package** to a location different than the one above, go to **3D Banner FX** from the **Settings** tab in your **WordPress Dashboard** and update the path accordingly
+4. Add `[3d-banner-fx][/3d-banner-fx]` where you want the Flash to show up in your post/page
+5. If you want to make the 3D Banner FX part of your theme, edit the template files and add `<?php 3dbannerfx_echo_embed_code(); ?>` where you want it to show up
+6. Go to [FlashXML.net](http://www.flashxml.net/ "Free Flash Components") and [customize your 3D Banner FX](http://www.flashxml.net/3d-banner.html "3D Banner FX") using the Live Demo. Generate the `settings.xml` text and use it to overwrite `wp-content/flashxml/3d-banner-fx/settings.xml`
+7. To use your own images, upload them to `wp-content/flashxml/3d-banner-fx/images` and update the `wp-content/flashxml/3d-banner-fx/images.xml` file accordingly
+
+= Additional settings file =
+
+To embed the 3D Banner FX more than once, you will need another settings file and (probably) another set of images. Let's assume your new file is called `settings2.xml`. Add `[3d-banner-fx settings="settings2.xml"][/3d-banner-fx]` where you want the Flash to show up in your post/page. If you made the Flash part of your theme, add the file name as **the first argument** of the `3dbannerfx_echo_embed_code()` function call (for example `<?php 3dbannerfx_echo_embed_code("settings2.xml"); ?>`).
+
+= Add as widget =
+To add the 3D Banner FX as a widget, go to **Widgets** from the **Appearance** tab in your *WordPress Dashboard*, then drag and drop the 3D Banner FX from **Available Widgets** to the widget area you want. You can specify a different `settings.xml` file and an alternative content for users without Adobe Flash Player from the widget's settings.
+
+= No Flash support text =
+
+To support visitors without Adobe Flash Player, you can provide alternative content by adding the text between `[3d-banner-fx]` and `[/3d-banner-fx]`. If you made the Flash part of your theme, add the text as **the second argument** of the `3dbannerfx_echo_embed_code()` function call (for example `<?php 3dbannerfx_echo_embed_code("","Alternative content"); ?>`).
+
+= If you have PHP4 =
+
+To make it work with PHP4, add `[3d-banner-fx width="600" height="300"][/3d-banner-fx]` where you want the Flash to show up in your post/page. If you made the Flash part of your theme, add the width and height as **the third and fourth argument** of the `3dbannerfx_echo_embed_code()` function call. Don't forget to provide your own width and height values, since 600 and 300 are just examples.
+
+= Getting rid of the FlashXML.net label =
+
+To remove the FlashXML.net label from the top-left corner you'll need to buy the [paid package](http://www.flashxml.net/3d-banner.html "3D Banner FX"). Once you'll do that, simply use the SWF file from the paid package to overwrite the SWF file from the `wp-content/flashxml/3d-banner-fx/` folder.
+
+== Screenshots ==
+
+1. The Live Demo on [FlashXML.net](http://www.flashxml.net/3d-banner.html "3D Banner FX") is the utility that helps easily customize your 3D Banner FX to fit all your needs.
